e71201b92f1711a74476d6168632fcc826fd38b9
espilker <ethanspilker@gmail.com>
2025-04-16 19:22:38 -0600

    Configured as PWA

--- movie-app/vite.config.ts
+++ movie-app/vite.config.ts
@@ -1,10 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/MovieWebsite",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["Atium.png"],
+      manifest: {
+        name: "Your App Name",
+        short_name: "AppName",
+        description: "Your app description",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     outDir: "dist",
   },
